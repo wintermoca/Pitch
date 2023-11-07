9d278e6cfe71b553adf167fae73063d4737ba3d6
권윤호 <yoonhoktx@gmail.com>
2023-11-07 14:04:34 +0900

    초기 커밋.

---
+++ PitchRaising.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PitchRaising.aspx.cs" Inherits="View.PitchRaising" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" style="text-align: center">
+        <asp:Label ID="lblTitle" runat="server" Text="피치를 올려라!"></asp:Label>
+        <br />
+        <asp:Button ID="btnPitchDown" runat="server" OnClick="btnPitchDown_Click" Text="한 음 내리기" />
+        <asp:Label ID="lblPitch" runat="server" Text="라"></asp:Label>
+        <asp:Button ID="btnPitchUp" runat="server" Text="한 음 올리기" />
+    </form>
+</body>
+</html>
---
+++ PitchRaising.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace View
+{
+    public partial class PitchRaising : System.Web.UI.Page
+    {
+        protected string[] PitchArray = { "도", "레", "미", "파", "솔", "라", "시" };
+        protected int Note;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) Note = (int)ViewState["Note"];
+            else Note = 5;
+        }
+        protected void Page_PrreRender(object sender, EventArgs e)
+        {
+            ViewState["Note"] = Note;
+        }
+        protected void btnPitchDown_Click(object sender, EventArgs e)
+        {
+            Note = (int)ViewState["Note"] - 1;
+            if (Note < 0) Note = 6;
+            ShowPitch();
+        }
+        protected void btnPitchUp_Click(object sender, EventArgs e)
+        {
+            Note = ((int)ViewState["Note"] + 1)%7;
+            
+            ShowPitch();
+        }
+        protected void ShowPitch()
+        {
+            lblPitch.Text = PitchArray[Note];
+        }
+
+       
+    }
+}
